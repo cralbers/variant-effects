a1e2b3e1388f2e0231840988528c61cef14449c3
Cora Albers <coraalbers@ascend-rw01.ten.osc.edu>
2026-08-11 19:25:50 -0400

    expanding ISM analysis

---
+++ osc/api_ism_1mb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --account=pas2905
+#SBATCH --partition=nextgen
+#SBATCH --job-name=lmna_ism_1mb
+#SBATCH --mem=128gb
+#SBATCH --time=10:00:00
+#SBATCH --mail-type=END,FAIL
+#SBATCH --output=logs/R-%x.%j.out
+#SBATCH --error=logs/R-%x.%j.err
+
+
+cd /users/PAS2905/coraalbers/ag/variant-effects/osc
+
+# load the necessary modules
+ml miniconda3/24.1.2-py310 cuda/12.9.1
+
+# activate the alphagenome conda environment
+conda activate py311
+
+python api_based_ism_recommended_scorers.py
+     
